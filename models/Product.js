5e740eef574250a6576ee8b1302569e12fb3c572
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-31 17:41:59 -0700

    added fields and rules to Category, Product, Tag, and Product Tag MODELS

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      type: DataTypes.INTERGER,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+      },
+    category_id: {
+      type: DataTypes.INTERGER,
+      // add reference categoty model's id
+      },
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTERGER,
+      // add reference to product models ID
+    },
+    tag_id: {
+      type: DataTypes.INTERGER,
+      //Add references to tag models ID
+    }
   },
   {
     sequelize,
